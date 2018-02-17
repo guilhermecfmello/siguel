cb7dd4fe175aa30eb62cb9067ed380bf023cc770
Guilherme Mello <guilhermecfmello@gmail.com>
2018-02-17 19:54:58 -0200

    Grafo implementado no t6, falta funcoes da cidade e funcao de menor caminho

--- Grafo.c
+++ Grafo.c
@@ -1,3 +1,315 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <math.h>
 #include "Grafo.h"
+
+
+typedef struct _grafo{
+  Hash vertices;
+  int size;
+}grafo;
+
+typedef struct _vertice{
+  char *id;
+  int grau;
+  double x;
+  double y;
+  double dist;
+  Lista arestas;
+  struct _vertice *prev;
+}vertice;
+
+typedef struct _aresta{
+  char *nome;
+  char *ldir;
+  char *lesq;
+  double cmp;
+  double vm;
+  double tempo;
+  Vertice ponta;
+}aresta;
+
+Grafo grafo_create(int size){
+  grafo *g;
+  g = malloc(sizeof(grafo));
+  if(g!=NULL){
+    g->size = size;
+    g->vertices = createHash(size);
+  }
+  return g;
+}
+
+Vertice grafo_create_vertice(char *id, double x, double y){
+  vertice *v;
+  v = malloc(sizeof(vertice));
+  if(v!=NULL){
+    v->grau = 0;
+    v->x = x;
+    v->y = y;
+    v->dist = -1;
+    v->prev = NULL;
+    v->arestas = createList();
+    v->id = malloc(sizeof(char)*(strlen(id)+1));
+    strcpy(v->id,id);
+  }
+  return v;
+}
+
+Aresta grafo_create_aresta(char *nome, char *ldir, char *lesq, double cmp, double vm){
+  aresta *a;
+
+  a = malloc(sizeof(aresta));
+  if(a!=NULL){
+    a->cmp = cmp;
+    a->vm = vm;
+    a->nome = malloc(sizeof(char)*(strlen(nome)+1));
+    a->ldir = malloc(sizeof(char)*(strlen(ldir)+1));
+    a->lesq = malloc(sizeof(char)*(strlen(lesq)+1));
+    strcpy(a->nome, nome);
+    strcpy(a->ldir, ldir);
+    strcpy(a->lesq, lesq);
+    if(vm!=0)
+      a->tempo = cmp/vm;
+    else
+      a->tempo = 0;
+  }
+  return a;
+}
+
+void grafo_insere_vertice(Grafo gra, Vertice V){
+  grafo *g;
+  vertice *v;
+
+  g = (grafo*) gra;
+  v = (vertice*) V;
+  hash_insert(g->vertices,v,v->id);
+}
+
+Vertice grafo_search_vertice_id(Grafo G, char *id){
+  grafo *g;
+  Vertice v;
+
+  g = (grafo*) G;
+  v = hash_search(g->vertices, id);
+  return v;
+}
+
+Lista grafo_get_allvertices(Grafo G){
+  int i;
+  grafo *g;
+  Lista list, newList;
+  Posic aux, reg, item;
+
+  g = (grafo*) G;
+  newList = createList();
+  for(i=0;i<g->size;i++){
+    list = hash_get_position(g->vertices,i);
+    aux = getFirst(list);
+    while(aux!=NULL){
+      reg = get(list,aux);
+      item = hash_get_reg(reg);
+      insert(newList,item);
+      aux = getNext(list,aux);
+    }
+  }
+  return newList;
+}
+
+int grafo_cond_adjacent(Grafo G, Vertice V1, Vertice V2){
+  vertice *v;
+  aresta *a;
+  Lista list;
+  Posic aux;
+
+  v = (vertice*) V1;
+  list = v->arestas;
+  aux = getFirst(list);
+  while(aux!=NULL){
+    a = (aresta*) get(list,aux);
+    if(a->ponta==V2)
+      return 1;
+    aux = getNext(list,aux);
+  }
+  return 0;
+}
+
+void grafo_insere_aresta(Grafo gra, Aresta A, Vertice V1, Vertice V2){
+  aresta *a;
+  vertice *v;
+
+  a = (aresta*) A;
+  v = (vertice*) V1;
+  a->ponta = V2;
+  insert(v->arestas,a);
+}
+
+Aresta grafo_get_aresta_ligacao(Grafo G, Vertice V1, Vertice V2){
+  vertice *v;
+  aresta *a;
+  Lista list;
+  Posic aux;
+
+  v = (vertice*) V1;
+  list = v->arestas;
+  aux = getFirst(list);
+  while(aux!=NULL){
+    a = (aresta*) get(list,aux);
+    if(a->ponta==V2)
+      return a;
+    aux = getNext(list,aux);
+  }
+  return NULL;
+}
+
+void grafo_remove_aresta_ligacao(Grafo G, Vertice V1, Vertice V2){
+  vertice *v;
+  aresta *a, *item;
+  Lista list;
+  Posic aux;
+
+  v = (vertice*) V1;
+  list = v->arestas;
+  aux = getFirst(list);
+  while(aux!=NULL){
+    item = (aresta*) get(list,aux);
+    if(item->ponta == V2){
+      remover(list,aux);
+      break;
+    }
+    aux = getNext(list,aux);
+  }
+}
+
+Lista grafo_get_allarestas(Grafo G){
+  int i, size;
+  grafo *g;
+  vertice *v;
+  RegH r;
+  Hash h;
+  Lista list, vertices, arestas;
+  Posic aux, aux2, auxL, a;
+
+  list = createList();
+  g = (grafo*) G;
+  for(i=0;i<g->size;i++){
+    vertices = hash_get_position(g->vertices,i);
+    aux = getFirst(vertices);
+    while(aux!=NULL){
+      auxL = get(vertices,aux);
+      v = (vertice*) hash_get_reg(auxL);
+
+      arestas = v->arestas;
+      aux2 = getFirst(arestas);
+      while(aux2!=NULL){
+        a = get(arestas,aux2);
+        /*printf("\n###TESTE: %s", a->nome);*/
+        insert(list,a);
+        aux2 = getNext(arestas,aux2);
+      }
+      aux = getNext(vertices,aux);
+    }
+  }
+  return list;
+}
+
+
+
+/*Getters*/
+
+int grafo_get_grafo_size(Grafo G){
+  grafo *g;
+  g = (grafo*) G;
+  return g->size;
+}
+
+char *grafo_get_vertice_id(Vertice V){
+  vertice *v;
+
+  v = (vertice*) V;
+  return v->id;
+}
+
+int grafo_get_vertice_grau(Vertice V){
+  vertice *v;
+
+  v = (vertice*) V;
+  return v->grau;
+}
+
+double grafo_get_vertice_x(Vertice V){
+  vertice *v;
+
+  v = (vertice*) V;
+  return v->x;
+}
+
+double grafo_get_vertice_y(Vertice V){
+  vertice *v;
+
+  v = (vertice*) V;
+  return v->y;
+}
+
+double grafo_get_vertice_dist(Vertice V){
+  vertice *v;
+
+  v = (vertice*) V;
+  return v->dist;
+}
+
+Lista grafo_get_vertice_arestas(Vertice V){
+  vertice *v;
+
+  v = (vertice*) V;
+  return v->arestas;
+}
+
+Vertice grafo_get_vertice_prev(Vertice V){
+  vertice *v;
+
+  v = (vertice*) V;
+  return v->prev;
+}
+
+char *grafo_get_aresta_nome(Aresta A){
+  aresta *a;
+
+  a = (aresta*) A;
+  return a->nome;
+}
+
+char *grafo_get_aresta_ldir(Aresta A){
+  aresta *a;
+
+  a = (aresta*) A;
+  return a->ldir;
+}
+
+char *grafo_get_aresta_lesq(Aresta A){
+  aresta *a;
+
+  a = (aresta*) A;
+  return a->lesq;
+}
+
+double grafo_get_aresta_cmp(Aresta A){
+  aresta *a;
+
+  a = (aresta*) A;
+  return a->cmp;
+}
+
+double grafo_get_aresta_vm(Aresta A){
+  aresta *a;
+
+  a = (aresta*) A;
+  return a->vm;
+}
+
+Vertice grafo_get_aresta_ponta(Aresta A){
+  aresta *a;
+
+  a = (aresta*) A;
+  return a->ponta;
+}
--- Grafo.h
+++ Grafo.h
@@ -1,44 +1,99 @@
 #ifndef GRAFO__H
 #define GRAFO__H
-
 #include "Lista.h"
+#include "Hash.h"
 
 typedef void* Grafo;
+typedef void* Vertice;
+typedef void* Aresta;
 
-Grafo grafo_create(int n);
-/*Cria um grafo contendo n vertices e zero arestas*/
+/*ESTE EH O GRAFO TAD. POR DEFINICAO, GRAFOS SAO UTILIZADOS PARA GERENCIAR E TRATAR
+DADOS QUE POSSUEM ALGUM TIPO DE RELACAO, ATRAVES DE VERTICES (OBJETO) E ARESTAS
+(RELACAO ENTRE OS OBJETOS).
+ESTE TAD PRODUZ GRAFOS DIRECIONADOS E PONDERADODS.
+COMO O GRAFO EH GERENCIADO POR LISTAS, EH RECOMENDADO QUE ESTE TAD SEJA UTILIZADO
+EM CASOS EM QUE O NUMERO DE ARESTAS NAO SEJA MUITO GRANDE, OU SEJA, O GRAU MAXIMO
+DE CADA VERTICE SEJA UM VALOR BAIXO.*/
 
-void grafo_insere_aresta(Grafo gra, int v1, int v2);
-/*v1 e v2 devem pertencer ao conjunto de vertices;
-Insere no grafo uma aresta nos vertices v1 e v2;
+Grafo grafo_create(int size);
+/*Cria um grafo e retorna-o*/
+
+Vertice grafo_create_vertice(char *id, double x, double y);
+/*Cria um vertice de nome ID, de coordenadas x e y no plano cartesiano*/
+
+Aresta grafo_create_aresta(char *nome, char *ldir, char *lesq, double cmp, double vm);
+/*Cria uma aresta identificada pelo nome, ldir e lesq sao os nomes das quadras a direita e esquerda
+do ponto atual, cmp eh o comprimento e vm a velocidade media.*/
+
+void grafo_insere_vertice(Grafo gra, Vertice v);
+/*Insere um vertice na grafo Gra.
+O vertice possui referencias x e y as suas posicoes no plano cartesiano invertido (SVG)*/
+
+void grafo_insere_aresta(Grafo gra, Aresta A, Vertice V1, Vertice V2);
+/*Insere uma aresta no grafo Gra que liga dois vertices definidos
+na criacao da aresta.*/
+
+Vertice grafo_search_vertice_id(Grafo G, char *id);
+/*Retorna o vertice de nome "Id". O Grafo G deve existir*/
+
+Lista grafo_get_allvertices(Grafo G);
+/*Retorna todos os vertices do grafo g em uma lista.*/
+
+Lista grafo_get_allarestas(Grafo G);
+/*Retorna todas as arestas do grafo G em uma lista*/
+
+Aresta grafo_get_aresta_ligacao(Grafo G, Vertice V1, Vertice V2);
+/*Retorna a aresta que liga o vertice V1 ao V2, caso nao haja ligacao entre eles, retorna NULL.*/
+
+void grafo_remove_aresta_ligacao(Grafo G, Vertice V1, Vertice V2);
+/*Caso haja uma aresta que liga V1 a V2, remove.*/
+
+int grafo_cond_adjacent(Grafo G, Vertice V1, Vertice V2);
+/*Verifica se os vertices V1 e V2 sao adjacentes, ou seja, possuem uma aresta de ligacao.
+Retorna 1 caso verdadeiro.
+Retorna 0 caso falso.
 */
 
-void grafo_define_info(Grafo gra, int v1, int v2, void *info);
-/*v1 e v1 devem pertencer ao grafo.
-Adiciona o conteudo info a aresta v1,v2 do grafo.
+Lista grafo_get_vertices_adjacentes(Grafo G, Vertice V);
+/*Retorna uma lista com todos vertices adjacentes ao vertice V, ou seja
+todos o vertices que possuem ao menos uma ligacao com V.
+Funcao ainda nao implementada.
 */
 
-void* grafo_getInfo(Grafo gra, int v1, int v2);
-/*v1 e v2 devem pertencer ao grafo.
-Retorna o conteudo pertencente a aresta v1,v2 do grafo.
-*/
+/*Getters*/
+int grafo_get_grafo_size(Grafo G);
+/*Retorna o tamanho do grafo G*/
+char *grafo_get_vertice_id(Vertice V);
+/*Retorna o Id do vertice V*/
+int grafo_get_vertice_grau(Vertice V);
+/*Retorna o grau (numero de arestas) do Vertice V*/
+double grafo_get_vertice_x(Vertice V);
+/*Retorna a posicao X do Vertice V*/
+double grafo_get_vertice_y(Vertice V);
+/*Retorna a posicao Y do Vertice V*/
+double grafo_get_vertice_dist(Vertice V);
+/*Retorna a distancia do Vertice V*/
+Lista grafo_get_vertice_arestas(Vertice V);
+/*Retorna uma Lista com todas as arestas do Vertice V*/
+Vertice grafo_get_vertice_prev(Vertice V);
+/*Retorna o prev do vertice V*/
+char *grafo_get_aresta_nome(Aresta A);
+/*Retorna o identificador nome da aresta A*/
+char *grafo_get_aresta_ldir(Aresta A);
+/*Retorna o nome da quadra a direita da aresta A*/
+char *grafo_get_aresta_lesq(Aresta A);
+/*Retorna o nome da quadra a esquerda da aresta A*/
+double grafo_get_aresta_cmp(Aresta A);
+/*Retorna o comprimento da Aresta A*/
+double grafo_get_aresta_vm(Aresta A);
+/*Retorna a velocidade media da Aresta A*/
+Vertice grafo_get_aresta_ponta(Aresta A);
+/*Retorna o vertice para onde a Aresta A aponta*/
 
-void grafo_remove_aresta(Grafo gra, int v1, int v2);
-/*v1 e v2 devem pertencer ao grafo.
-Remove a aresta correspondente aos vertices v1 e v2 do grafo.
-*/
+/*FUNCOES TESTE*/
+char *grafo_vertice_get_id(Vertice v);
 
-int grafo_adjacente(Grafo gra, int v1, int v2);
-/*v1 e v2 devem pertencer ao grafo.
-retorna 1 se v1 e v2 forem vizinhos.
-retorna 0 caso contrario.
-*/
+double grafo_vertice_get_x(Vertice v);
 
-Lista grafo_adjacentes(Grafo gra, int v);
-/*v deve pertencer ao grafo
-Retorna uma lista contento todos os vertices adjacentes ao vertice v;
-*/
-
-
-
+void grafo_imprime_arestas_do_vertice(Vertice v);
 #endif
